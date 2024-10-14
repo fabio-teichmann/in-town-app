f738d8e00d81118115f94e3a15bca2a1246b680c
Fabio Teichmann <fteichmann@veritytracking.com>
2024-10-14 13:45:23 +0200

    feat(#17-users-page): Added users page and added to navigation
    Closes #17

--- in_town_app.py
+++ in_town_app.py
@@ -10,7 +10,8 @@ home_page = st.Page("home.py", title="Home", icon=":material/home:")
 trips_page = st.Page("trips/trips.py", title="Overview", icon=":material/travel:")
 trips_edit = st.Page("trips/edit_trip.py", title="Edit Trip", icon=":material/edit:")
 groups_page = st.Page("groups.py", title="Groups", icon=":material/groups:")
+users_page = st.Page("groups/users.py",title="Users", icon=":material/person:")
 
-pg = st.navigation({"Home": [home_page], "Trips": [trips_page, trips_edit], "Groups": [groups_page]})
+pg = st.navigation({"Home": [home_page], "Trips": [trips_page, trips_edit], "Groups": [groups_page, users_page]})
 
 pg.run()
