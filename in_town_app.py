c26df528bf87d436258d9627e21465ff7a66a695
Fabio Teichmann <fteichmann@veritytracking.com>
2024-10-15 08:03:16 +0200

    feat(#21-user-categories-page): Added page + navigation

--- in_town_app.py
+++ in_town_app.py
@@ -10,8 +10,17 @@ home_page = st.Page("home.py", title="Home", icon=":material/home:")
 trips_page = st.Page("trips/trips.py", title="Overview", icon=":material/travel:")
 trips_edit = st.Page("trips/edit_trip.py", title="Edit Trip", icon=":material/edit:")
 groups_page = st.Page("groups.py", title="Groups", icon=":material/groups:")
-users_page = st.Page("groups/users.py",title="Users", icon=":material/person:")
+users_page = st.Page("groups/users.py", title="Users", icon=":material/person:")
+user_category_page = st.Page(
+    "groups/user_categories.py", title="User Categories", icon=":material/badge:"
+)
 
-pg = st.navigation({"Home": [home_page], "Trips": [trips_page, trips_edit], "Groups": [groups_page, users_page]})
+pg = st.navigation(
+    {
+        "Home": [home_page],
+        "Trips": [trips_page, trips_edit],
+        "Groups": [groups_page, users_page, user_category_page],
+    }
+)
 
 pg.run()
