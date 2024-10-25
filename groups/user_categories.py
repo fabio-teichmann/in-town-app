55c24dc7b70cf05fce1daa54c1ad3a8a3ef9505a
Fabio Teichmann <fteichmann@veritytracking.com>
2024-10-25 21:00:52 +0300

    refactor(#31-json): user categories to JSON; adapted CRUD functions accordingly

--- groups/user_categories.py
+++ groups/user_categories.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import pandas as pd
 
 
 def get_categories():
     if "user_categories" not in st.session_state:
-        return pd.DataFrame()
+        return [{}]
     return st.session_state.user_categories
 
 
@@ -14,18 +13,15 @@ def create_new_category():
     categories = get_categories()
     button = st.button("Add Category")
     if button:
-        id = max(categories["id"]) + 1
-        category = pd.DataFrame(index=[0], data={"id": id, "Category": category})
-        st.session_state.user_categories = pd.concat(
-            [categories, category], ignore_index=True
-        )
-        st.write(st.session_state.user_categories)
+        id = len(categories) + 1
+        category = {"id": id, "Category": category}
+        st.session_state.user_categories.append(category)
         st.rerun()
 
 
 def display_cat(option):
-    for _, cat in get_categories().iterrows():
-        if option == cat["id"]:
+    for cat in get_categories():
+        if option["id"] == cat["id"]:
             return f"{cat['Category']}"
 
 
@@ -33,15 +29,18 @@ def display_cat(option):
 def edit_category():
     categories = get_categories()
 
-    cat_id = st.selectbox(
+    selected_category = st.selectbox(
         label="Select Category", options=categories, format_func=display_cat
     )
-    cat_name = categories.loc[categories["id"] == cat_id]["Category"].iloc[0]
-    new_category = st.text_input(label="Category Name", value=cat_name)
+    new_category = st.text_input(
+        label="Category Name", value=selected_category["Category"]
+    )
 
     button = st.button("Edit Category")
     if button:
-        categories.loc[categories["id"] == cat_id, "Category"] = new_category
+        for cat in categories:
+            if cat["id"] == selected_category["id"]:
+                cat["Category"] = new_category
         st.session_state.user_categories = categories
         st.rerun()
 
@@ -49,13 +48,13 @@ def edit_category():
 @st.dialog("Delete category")
 def delete_category():
     categories = get_categories()
-    cat_id = st.selectbox(
+    selected_category = st.selectbox(
         label="Select Category", options=categories, format_func=display_cat
     )
 
     button = st.button("Delete Category")
     if button:
-        categories = categories.loc[categories["id"] != cat_id]
+        categories.remove(selected_category)
         st.session_state.user_categories = categories
         st.rerun()
 
@@ -69,9 +68,16 @@ right.button("Delete category :material/delete:", on_click=delete_category)
 
 if "user_categories" not in st.session_state:
     # add dummy data
-    dummy_categories = pd.DataFrame(
-        data={"id": [1, 2], "Category": ["Family", "Besties"]}
-    )
+    dummy_categories = [
+        {
+            "id": 1,
+            "Category": "Family",
+        },
+        {
+            "id": 2,
+            "Category": "Besties",
+        },
+    ]
     st.session_state.user_categories = dummy_categories
 
 categories = get_categories()
