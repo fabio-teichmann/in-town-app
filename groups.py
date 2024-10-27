207b341a38695e20cc9017fba08704abeca66db9
Fabio Teichmann <fteichmann@veritytracking.com>
2024-10-27 09:13:23 +0200

    feat(#4): basic layout for groups
    Closes #4

--- groups.py
+++ groups.py
@@ -1,11 +1,31 @@
 import streamlit as st
 
+
+def create_group():
+    pass
+
+
+def edit_group():
+    pass
+
+
+def delete_group():
+    pass
+
+
 st.title("My Groups")
 
-display = ("male", "female")
+left, mid, right, _ = st.columns(4)
 
-options = list(range(len(display)))
+left.button("Create group :material/group_add:", on_click=create_group)
+mid.button("Edit group :material/edit:", on_click=edit_group)
+right.button("Delete group :material/delete:", on_click=delete_group)
 
-value = st.selectbox("gender", options, format_func=lambda x: display[x])
-
-st.write(value)
+empty_groups = [
+    {
+        "id": None,
+        "name": None,
+        "members": [],
+    }
+]
+st.table(empty_groups)
